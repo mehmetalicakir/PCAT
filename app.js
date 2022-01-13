f2d446aea7ee598aa886be15a554e5554c1bf097
Mehmet Ali ÇAKIR <cakiroglu_m.ali@hotmail.com>
2022-01-13 19:29:49 +0300

    middleware

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const { path } = require('express/lib/application');
 const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware log 1');
+  next();
+};
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log 2');
+  next();
+};
+
+// Middlewares
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
 const port = 3000;
 
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        name: "photo",
-        description: "Photo description"
-    }
-  res.send('İstek Alındı.');
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
